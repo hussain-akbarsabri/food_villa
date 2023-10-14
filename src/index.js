5b022b5e9a37c75c753e27d6608a0492ed510731
hussain-akbarsabri <hussain.akbarsabri@gmail.com>
2023-10-14 02:10:06 -0500

    installed tailwind css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<h1>Hello, world</h1>);
+root.render(<App />);
